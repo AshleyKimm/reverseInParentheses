6fb25e05a5390e6625426ac0c3086309ba58598b
Hyunseo Ashley Kim <74394337+Kimm910@users.noreply.github.com>
2022-09-19 23:26:12 -0400

    Update reverseInParentheses.cpp

--- reverseInParentheses.cpp
+++ reverseInParentheses.cpp
@@ -1,35 +1,41 @@
 #include <iostream>
 
-
 int main();
 std::string solution(std::string);
 
 int main() {
     std::string input;
-    std::cout << "Enter a string with parentheses: " << std::endl;
+
+    std::cout << "Enter a string with parentheses: ";
     std::cin >> input;
     std::string answer = solution(input);
-    std::cout << answer << std::endl;
+    std::cout << "After reversed: " << answer << std::endl;
 }
+
+
 std::string solution(std::string inputString) {
+    
     for (int i{0}; i < inputString.size(); i ++) {
         
+        // Searches for the index of ')' and moves to the left to
+        // search for '('
         if (inputString[i] == ')') {
             int close = i;
-            std::cout << "Before erased : " << inputString << std::endl;
             inputString.erase(close, 1);
             for (int open {close - 1}; open >= 0; open --) {
                 
+                // Searches for the index of '('
                 if (inputString[open] == '(') {
                     for (int k{1}; (open + k) < (close - k); k ++) {
-                            char temp{};
-                            temp = inputString[close - k];
-                            inputString[close - k] = inputString[open + k];
-                            inputString[open + k] = temp;
+
+                        // Bubblesort method to reverse letters inside brackets
+                        char temp{};
+                        temp = inputString[close - k];
+                        inputString[close - k] = inputString[open + k];
+                        inputString[open + k] = temp;
                     }
                     inputString.erase(open, 1);
                     break;
-                    std::cout << "after erased : " << inputString << std::endl;
                 }
                 --i;
             }
